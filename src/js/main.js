388f9fae35eb7111f88d8827ba35ab1c2229f849
HamaHs <lusuy1995@gmail.com>
2024-06-28 20:34:13 +0200

    some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -4,30 +4,22 @@ const cards = document.querySelectorAll(".card");
 const tabs = document.querySelectorAll(".tab");
 const heroImage = document.querySelector(".hero__image path");
 
-const colors = [
-   "rgb(90, 88, 207)",
-   "rgb(64, 139, 252)",
-   "rgb(79, 98, 88)",
-   "rgb(249, 210, 112)",
-   "rgb(207, 109, 88)",
-];
+function isInViewport(element) {
+   const { top, left, bottom, right } = element.getBoundingClientRect();
+   return (
+      top >= 0 &&
+      left >= 0 &&
+      bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      right <= (window.innerWidth || document.documentElement.clientWidth)
+   );
+}
 
 tabs.forEach((tab, i) => {
+   const tabIcon = tab.querySelector("figure");
+
    tab.addEventListener("click", (e) => {
       e.preventDefault();
       cards[i].scrollIntoView({ behavior: "smooth", block: "center" });
-      heroImage.style.transition = "all 0.3s";
-      heroImage.setAttribute("fill", colors[i]);
+      heroImage.setAttribute("fill", window.getComputedStyle(tabIcon).backgroundColor);
    });
 });
-
-document.querySelector("body").addEventListener("scroll", () => {
-   console.log();
-});
-
-//function isInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   return (
-//      rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-//   );
-//}
