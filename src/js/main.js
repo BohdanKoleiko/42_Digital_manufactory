e4a584c0dcf93964626e240d6369a24427965eca
HamaHs <lusuy1995@gmail.com>
2024-06-30 06:18:21 +0200

    made color of svg react to card

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const cards = document.querySelectorAll(".card");
 const tabs = document.querySelectorAll(".tab");
 const heroImage = document.querySelector(".hero__image path");
 
-function isInViewport(element) {
+const isInViewport = function (element) {
    const { top, left, bottom, right } = element.getBoundingClientRect();
    return (
       top >= 0 &&
@@ -12,14 +12,39 @@ function isInViewport(element) {
       bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       right <= (window.innerWidth || document.documentElement.clientWidth)
    );
-}
+};
+
+const removeActiveClass = function () {
+   tabs.forEach((tab) => {
+      tab.classList.remove("tab__active");
+   });
+};
+
+const addActiveClass = function (index) {
+   tabs[index].classList.add("tab__active");
+};
+
+const setBackgroundColor = function (index) {
+   const tabIcon = tabs[index].querySelector("figure");
+   heroImage.setAttribute("fill", window.getComputedStyle(tabIcon).backgroundColor);
+   addActiveClass(index);
+};
+setBackgroundColor(0);
 
 tabs.forEach((tab, i) => {
-   const tabIcon = tab.querySelector("figure");
-
    tab.addEventListener("click", (e) => {
       e.preventDefault();
       cards[i].scrollIntoView({ behavior: "smooth", block: "center" });
-      heroImage.setAttribute("fill", window.getComputedStyle(tabIcon).backgroundColor);
+      removeActiveClass();
+      addActiveClass(i);
+   });
+});
+
+document.querySelector("body").addEventListener("scroll", () => {
+   cards.forEach((card, i) => {
+      if (isInViewport(card)) {
+         removeActiveClass();
+         setBackgroundColor(i);
+      }
    });
 });
