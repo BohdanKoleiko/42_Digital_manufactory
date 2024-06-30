e09c1703780c2f4e2971f372eba0112540f1c2a5
HamaHs <lusuy1995@gmail.com>
2024-06-30 23:35:38 +0200

    optimize and remade script

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,66 @@
 "use strict";
 
-const cards = document.querySelectorAll(".card");
-const tabs = document.querySelectorAll(".tab");
-const heroImage = document.querySelector(".hero__image path");
+document.addEventListener("DOMContentLoaded", () => {
+   const cards = document.querySelectorAll(".card");
+   const tabs = document.querySelectorAll(".tab");
+   const heroImage = document.querySelectorAll("[class|='hero__image'] path");
+   let widthOfWindow = window.innerWidth || document.documentElement.clientWidth;
 
-const isInViewport = function (element) {
-   const { top, left, bottom, right } = element.getBoundingClientRect();
-   return (
-      top >= 0 &&
-      left >= 0 &&
-      bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      right <= (window.innerWidth || document.documentElement.clientWidth)
-   );
-};
+   const isInViewport = function (element) {
+      const { top, left, bottom, right } = element.getBoundingClientRect();
+      return (
+         top >= 0 &&
+         left >= 0 &&
+         bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+         right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+   };
 
-const removeActiveClass = function () {
-   tabs.forEach((tab) => {
-      tab.classList.remove("tab__active");
-   });
-};
+   const removeActiveClass = function () {
+      tabs.forEach((tab, i) => {
+         tab.classList.remove("tab__active");
+         cards[i].classList.remove("hero__card_active");
+      });
+   };
 
-const addActiveClass = function (index) {
-   tabs[index].classList.add("tab__active");
-};
+   const addActiveClass = function (index) {
+      tabs[index].classList.add("tab__active");
+      cards[index].classList.add("hero__card_active");
+   };
 
-const setBackgroundColor = function (index) {
-   const tabIcon = tabs[index].querySelector("figure");
-   heroImage.setAttribute("fill", window.getComputedStyle(tabIcon).backgroundColor);
-   addActiveClass(index);
-};
-setBackgroundColor(0);
+   const setBackgroundColor = function (index) {
+      const tabIcon = tabs[index].querySelector("figure");
+      heroImage.forEach((img) => {
+         img.setAttribute("fill", window.getComputedStyle(tabIcon).backgroundColor);
+      });
+   };
 
-tabs.forEach((tab, i) => {
-   tab.addEventListener("click", (e) => {
-      e.preventDefault();
-      cards[i].scrollIntoView({ behavior: "smooth", block: "center" });
+   const initialization = function (index = 0) {
       removeActiveClass();
-      addActiveClass(i);
-   });
-});
+      addActiveClass(index);
+      setBackgroundColor(index);
+   };
+   initialization();
 
-document.querySelector("body").addEventListener("scroll", () => {
-   cards.forEach((card, i) => {
-      if (isInViewport(card)) {
-         removeActiveClass();
-         setBackgroundColor(i);
+   tabs.forEach((tab, i) => {
+      tab.addEventListener("click", (e) => {
+         e.preventDefault();
+         cards[i].scrollIntoView({ behavior: "smooth", block: "center" });
+         initialization(i);
+      });
+   });
+
+   window.addEventListener("resize", () => {
+      widthOfWindow = window.innerWidth || document.documentElement.clientWidth;
+   });
+
+   document.querySelector("body").addEventListener("scroll", () => {
+      if (widthOfWindow >= 992) {
+         cards.forEach((card, i) => {
+            if (isInViewport(card)) {
+               initialization(i);
+            }
+         });
       }
    });
 });
